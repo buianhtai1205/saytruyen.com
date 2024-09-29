ee9c352c75a53117d99262ed2a8bd941740bb27d
buianhtai1205 <buianhtai2017tq@gmail.com>
2024-09-29 15:32:57 +0700

    Refactor: refactor source according to microservice

---
+++ saytruyen-microservice/story-service/src/main/java/vn/com/saytruyen/story_service/controller/ChapterController.java
@@ -0,0 +1,10 @@
+package vn.com.saytruyen.story_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/chapter")
+public interface ChapterController {
+
+}
